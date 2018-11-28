a38a633232cbcbfbb236226ceced17813e281251
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-28 10:39:09 -0500

    done

---
+++ other/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+
+const App = () => (
+  <div>
+    <p>App</p>
+  </div>
+)
+
+export default App
---
+++ other/components/Gallery.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Gallery = () => (
+  <p>Gallery</p>
+)
+
+export default Gallery;
